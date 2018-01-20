587f1c89ded52b480b12c55836d0ca6217d09cd7
Elkien3 <Elkien3@users.noreply.github.com>
2018-01-20 09:20:13 -0600

    Fix default tags being visible
    the code before worked for local server, but for some reason on Persisten Kingdoms server it would be visible for 10 seconds, turn off, then turn on for 10 seconds.

--- init.lua
+++ init.lua
@@ -70,12 +70,6 @@ minetest.register_entity("playertag:tag", nametag)
 
 local function step()
 	for _, player in pairs(minetest.get_connected_players()) do
-		if not player.tag then
-			player:set_nametag_attributes({
-				color = {a = 0, r = 0, g = 0, b = 0}
-			})
-		end
-
 		if nametags[player:get_player_name()]:get_luaentity() == nil then
 			add_tag(player)
 			--minetest.chat_send_all("tag made for "..player:get_player_name())
@@ -87,6 +81,14 @@ local function step()
 	minetest.after(10, step)
 end
 minetest.after(10, step)
+
+minetest.register_globalstep(function(player)
+	for _, player in pairs(minetest.get_connected_players()) do
+		player:set_nametag_attributes({
+			color = {a = 0, r = 0, g = 0, b = 0}
+		})
+	end
+end)
 
 minetest.register_on_joinplayer(function(player)
 	if not player.tag then
